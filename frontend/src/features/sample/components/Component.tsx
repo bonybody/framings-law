d84f23c72bfc65fd451be05e6abde675488d9517
nishimura <11kakeru30@gmail.com>
2022-10-25 16:44:53 +0900

    create: sample featureの作成

---
+++ frontend/src/features/sample/components/Component.tsx
@@ -0,0 +1,7 @@
+export type ComponentProps = {
+  //
+}
+
+export const Component = ({}: ComponentProps) => {
+  return <div>Component</div>
+}
---
+++ frontend/src/features/sample/pages/Sample.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+import { Component } from '../components/Component'
+
+const Sample: NextPage = () => {
+  return <Component />
+}
+
+export default Sample
