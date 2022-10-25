ebc5a5186c0cb50754513a002b40a130bed4131d
nishimura <11kakeru30@gmail.com>
2022-10-25 13:53:21 +0900

    lint:fixを実行

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,9 @@
+import { css } from '@emotion/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
-import { css } from '@emotion/react'
+import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
