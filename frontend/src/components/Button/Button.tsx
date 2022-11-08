670de0771c4c1f4f489d4236c8bac13791ffb74e
makotty <makotty.mako@gmail.com>
2022-11-08 15:45:13 +0900

    Create : Button Component

---
+++ frontend/src/components/Button/Button.tsx
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled'
+import type { ReactNode } from 'react'
+
+export type ButtonProps = {
+  children: ReactNode
+  isActive?: boolean
+  isDisable?: boolean
+}
+
+export const Button = ({ isActive, isDisable, children }: ButtonProps) => {
+  return (
+    <ButtonWrap
+      isActive={isActive}
+      isDisable={isDisable}
+      onClick={() => {
+        console.log('click')
+      }}
+    >
+      {children}
+    </ButtonWrap>
+  )
+}
+
+type ButtonStyleProps = {
+  isActive?: boolean
+  isDisable?: boolean
+}
+
+const ButtonWrap = styled.button<ButtonStyleProps>`
+  cursor: pointer;
+  font-size: 20px;
+  line-height: 20px;
+  padding: 20px 0;
+  min-width: 240px;
+  border: solid 5px #000;
+  box-sizing: border-box;
+  background-color: ${(props) => (props.isActive ? '#FF8A00' : '#fff')};
+  opacity: ${(props) => (props.isDisable ? 0.6 : 1)};
+`
