95e096693f830a8070819f833632dc5ef98bb063
kaho <57692753+tanakaho@users.noreply.github.com>
2022-11-17 00:08:12 +0900

    修正しました

--- frontend/src/components/Elements/PostCard/PostCard.tsx
+++ frontend/src/components/Elements/PostCard/PostCard.tsx
@@ -3,36 +3,50 @@
 import styled from '@emotion/styled'
 
 export type PostCardProps = {
-  // 投稿日、年齢、性別
-  postInfo_text: string
+  // 投稿日
+  PostedDate: string
+  // 年齢
+  Age: string
+  // 性別
+  Gender: string
   // 投稿内容
   content: string
   // 選択できる状態か否か
-  canSelect: boolean
+  isSelect: boolean
   // 炎上カードだと思ったか
-  judgBurst: boolean
+  isFlamePost: boolean
 }
 
-export const PostCard = ({ postInfo_text, content, canSelect, judgBurst }: PostCardProps) => {
+export const PostCard = ({
+  PostedDate,
+  Age,
+  Gender,
+  content,
+  isSelect,
+  isFlamePost
+}: PostCardProps) => {
   return (
-    <Card judgBurst={judgBurst} canSelect={canSelect}>
-      <PostInfo>{postInfo_text}</PostInfo>
-      <PostContent_parent>
-        <PostContent>{content}</PostContent>
-      </PostContent_parent>
+    <Card isFlamePost={isFlamePost} isSelect={isSelect}>
+      <PostInfo>
+        <PostText>{PostedDate + '/' + Age + '代/' + Gender}</PostText>
+      </PostInfo>
+      <PostContent>
+        <PostText>{content}</PostText>
+      </PostContent>
     </Card>
   )
 }
 
 // 投稿カード
-const Card = styled.div<{ judgBurst: boolean; canSelect: boolean }>`
+const Card = styled.div<{ isFlamePost: boolean; isSelect: boolean }>`
   position: relative;
   width: 100%;
   min-height: 140px;
-  background-color: ${(props) => (props.judgBurst ? '#FFAA2C' : '#FFFFFF')};
+  background-color: ${(props) => (props.isFlamePost ? '#FFAA2C' : '#FFFFFF')};
   border: solid 5px #2b2b2b;
-  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-  opacity: ${(props) => (props.canSelect ? 'none' : '0.5')};
+  box-shadow: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  opacity: ${(props) => (props.isSelect ? 'none' : '0.5')};
+  box-sizing: border-box;
 
   // 炎
   &::before {
@@ -43,7 +57,7 @@ const Card = styled.div<{ judgBurst: boolean; canSelect: boolean }>`
     right: 0;
     bottom: 0;
     content: '';
-    background-image: ${(props) => (props.judgBurst ? "url('/fire.png')" : 'none')};
+    background-image: ${(props) => (props.isFlamePost ? "url('/fire.png')" : 'none')};
     background-size: cover;
     z-index: -1;
   }
@@ -51,15 +65,18 @@ const Card = styled.div<{ judgBurst: boolean; canSelect: boolean }>`
 
 // 投稿日、年齢、性別
 const PostInfo = styled.p`
-  color: #2b2b2b;
   margin: 0;
   padding: 18px 0 0 18px;
+`
+// テキスト
+const PostText = styled.p`
+  margin: 0;
+  color: #2b2b2b;
   font-style: normal;
   font-weight: 400;
   font-size: 14px;
   line-height: 20px;
 `
-
 // 投稿内容
 const PostContent = styled.p`
   color: #2b2b2b;
@@ -68,13 +85,4 @@ const PostContent = styled.p`
   padding-top: 12px;
   padding-bottom: 29px;
   display: inline-block;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 20px;
-`
-
-// 投稿内容にtextAlignをかける為の親要素
-const PostContent_parent = styled.div`
-  text-align: left;
 `
