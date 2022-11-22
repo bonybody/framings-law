224689fc4e41433ce1c2e378517e570ab5fdfab7
makotty <makotty.mako@gmail.com>
2022-11-22 17:04:14 +0900

    Create : Theming

---
+++ frontend/src/theme/index.ts
@@ -0,0 +1,34 @@
+export const theme = {
+  fonts: {
+    mainFont: "'Yusei Magic', sans-serif",
+    sizes: {
+      xs: '14px',
+      sm: '16px',
+      body1: '20px',
+      body2: '24px',
+      md: '32px',
+      lg: '40px',
+      xl: '48px'
+    }
+  },
+  colors: {
+    primary: {
+      main: '#FFAA2C',
+      dark: '#FF8A00'
+    },
+    secondary: {
+      main: '#33CE68',
+      dark: '#0A8232'
+    },
+    background: {
+      main: '#DF114F',
+      right: '#D2104A'
+    },
+    border: {
+      black: '#2B2B2B'
+    },
+    gray: '#D9D9D9',
+    white: '#FFFFFF'
+  },
+  shadow: '0 4px 4px 0 rgba(0, 0, 0, 0.25)'
+}
