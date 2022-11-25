0dcc0e7675d51f89c7a459647c5c341bf1c7be53
kaho <57692753+tanakaho@users.noreply.github.com>
2022-11-25 14:35:00 +0900

    icon prop

--- frontend/src/components/PostListToggleTab/Tab.tsx
+++ frontend/src/components/PostListToggleTab/Tab.tsx
@@ -7,12 +7,14 @@ export type TabProps = {
   isActive: boolean
   // テキスト
   tabName: string
+  // タブアイコン
+  icon: string
 }
 
-export const Tab = ({ isSelect, isActive, tabName }: TabProps) => {
+export const Tab = ({ isSelect, isActive, tabName, icon }: TabProps) => {
   return (
     <SwitchBtn isSelect={isSelect} isActive={isActive}>
-      <BtnText>{tabName}</BtnText>
+      <BtnText icon={icon}>{tabName}</BtnText>
     </SwitchBtn>
   )
 }
@@ -22,13 +24,13 @@ const SwitchBtn = styled.div<{ isSelect: boolean; isActive: boolean }>`
   box-sizing: border-box;
   width: 49.1%;
   height: 32px;
-  background-color: ${(prop) => (prop.isSelect ? '#FF8A00' : '#D9D9D9')};
+  background-color: ${(props) => (props.isSelect ? '#FF8A00' : '#D9D9D9')};
   border: 3px solid #2b2b2b;
   opacity: ${(props) => (props.isActive ? 'none' : '0.5')};
 `
 
 // テキスト
-const BtnText = styled.p`
+const BtnText = styled.p<{ icon: string }>`
   margin: 0;
   font-style: normal;
   font-weight: 400;
@@ -39,4 +41,17 @@ const BtnText = styled.p`
   /* text-shadow: 1px 0 0 #2b2b2b, -1px 0 0 #2b2b2b, 0 -1px 0 #2b2b2b, 0 1px 0 #2b2b2b,
     1px 1px 0 #2b2b2b, -1px -1px 0 #2b2b2b, -1px 1px 0 #2b2b2b, 1px -1px 0 #2b2b2b; */
   -webkit-text-stroke: 0.5px #2b2b2b;
+
+  // アイコン
+  &::before {
+    display: inline-block;
+    width: ${(props) => (props.icon == "url('/cards.png')" ? '30px' : '16.95px')};
+    height: ${(props) => (props.icon == "url('/cards.png')" ? '13.71px' : '20.87px')};
+    right: 0;
+    bottom: 0;
+    content: '';
+    background-image: ${(props) => props.icon};
+    background-size: cover;
+    z-index: -1;
+  }
 `
