c27c90af632738e37aab11877d98930ae54a3609
makotty <makotty.mako@gmail.com>
2022-11-29 17:50:15 +0900

    Add : Global style

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,13 +1,13 @@
-import '../styles/globals.css'
-
-import { ThemeProvider } from '@emotion/react'
+import { Global, ThemeProvider } from '@emotion/react'
 import type { AppProps } from 'next/app'
 
-import { theme } from '@/theme'
+import { styles } from '@/styles/global'
+import { theme } from '@/themes/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Global styles={styles} />
       <Component {...pageProps} />
     </ThemeProvider>
   )
