d188b2667a0c79c55c5f8762de55b9cc3b2ed3ed
kaho <57692753+tanakaho@users.noreply.github.com>
2022-11-29 11:13:21 +0900

    一番上の親からのprop

--- frontend/src/components/PostListToggleTab/PostListToggleTab.tsx
+++ frontend/src/components/PostListToggleTab/PostListToggleTab.tsx
@@ -3,14 +3,37 @@ import styled from '@emotion/styled'
 import { Tab } from './Tab'
 
 export type PostListToggleTabProps = {
-  //
+  // "すべて"がactiveか
+  isActiveAll: boolean
+  // "炎上"がactiveか
+  isActiveFlame: boolean
+  // "すべて"が選択されているか
+  isSelectAll: boolean
+  // "炎上"が選択されているか
+  isSelectFlame: boolean
 }
 
-export const PostListToggleTab = ({}: PostListToggleTabProps) => {
+export const PostListToggleTab = ({
+  isActiveAll,
+  isActiveFlame,
+  isSelectAll,
+  isSelectFlame
+}: PostListToggleTabProps) => {
+  // const [ddd, handlClick] = useState("")
   return (
     <TabBox>
-      <Tab isSelect={true} isActive={false} tabName="すべて" icon="url('/cards.png')" />
-      <Tab isSelect={false} isActive={false} tabName="炎上" icon="url('/fire.png')" />
+      <Tab
+        isSelect={isSelectAll}
+        isActive={isActiveAll}
+        tabName="すべて"
+        icon="url('/cards.png')"
+      />
+      <Tab
+        isSelect={isSelectFlame}
+        isActive={isActiveFlame}
+        tabName="炎上"
+        icon="url('/fire.png')"
+      />
     </TabBox>
   )
 }
