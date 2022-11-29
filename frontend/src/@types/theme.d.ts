f84e7bf2e3b63d59171f070a54b06b4c90e5aa38
makotty <makotty.mako@gmail.com>
2022-11-29 14:51:44 +0900

    Themeの型定義

---
+++ frontend/src/@types/theme.d.ts
@@ -0,0 +1,11 @@
+import '@emotion/react'
+
+import { Colors, Fonts } from '@/themes/theme'
+
+declare module '@emotion/react' {
+  interface Theme {
+    fonts: Fonts
+    colors: Colors
+    shadow: string
+  }
+}
