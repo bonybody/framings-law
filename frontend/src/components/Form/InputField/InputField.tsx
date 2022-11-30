8d3e51f01f932961e10ab211444e37e07a6d8656
makotty <makotty.mako@gmail.com>
2022-11-30 14:53:22 +0900

    Create : InputField

---
+++ frontend/src/components/Form/InputField/InputField.tsx
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled'
+import { UseFormRegisterReturn } from 'react-hook-form'
+
+import { Input } from '@/components/Elements'
+
+export type InputFieldProps = {
+  label: string
+  placeholder: string
+  register: UseFormRegisterReturn
+  backgroundColor: string
+  minWidth?: string
+}
+
+export const InputField = ({
+  label,
+  register,
+  placeholder,
+  backgroundColor,
+  minWidth
+}: InputFieldProps) => {
+  return (
+    <>
+      <Label>{label}</Label>
+      <Input
+        register={register}
+        placeholder={placeholder}
+        backgroundColor={backgroundColor}
+        minWidth={minWidth}
+      />
+    </>
+  )
+}
+
+const Label = styled.label`
+  color: white;
+  text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;
+`
