c927ba0f44096ddeaab029de3d914f02bc217606
makotty <makotty.mako@gmail.com>
2022-12-15 13:35:50 +0900

    Fix : Global Styles

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -3,5 +3,10 @@ import { css } from '@emotion/react'
 export const styles = css`
   html {
     font-family: 'Yusei Magic', sans-serif;
+    background-color: #d2104a;
+  }
+
+  body {
+    margin: 0;
   }
 `
