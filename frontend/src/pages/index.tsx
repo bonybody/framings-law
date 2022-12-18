098c9f5c4ba427047f1944886941b4b49dd830ae
kaho <57692753+tanakaho@users.noreply.github.com>
2022-12-18 21:35:05 +0900

    レイアウトなるべく崩れないように修正

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { keyframes } from '@emotion/react'
 import styled from '@emotion/styled'
 import type { NextPage } from 'next'
 
@@ -26,7 +27,7 @@ const Container = styled.div`
 const Logo = styled.img`
   position: absolute;
   width: 12.5vw;
-  height: 12.5vw;
+  height: 12.5vh;
   top: 6.5vh;
   right: 6.5vw;
 `
@@ -35,10 +36,13 @@ const Logo = styled.img`
 const Anne = styled.div`
   position: absolute;
   width: 100%;
-  height: 95%;
-  top: 6.5vh;
-  background-size: 110%;
+  height: 100%;
+  /* aspect-ratio: 9/20; */
+  /* top: 6.5vh; */
   background-image: url('/anne.svg');
+  background-position: top calc(10px - 0%) right 0%;
+  background-size: calc(1px + 110%) calc(1px + 120%);
+  background-repeat: no-repeat;
 `
 
 // タイトルロゴ
@@ -51,121 +55,37 @@ const Title = styled.img`
   top: 26vh;
 `
 
+// ボタン点滅
+const Flashing = keyframes`
+  0%,
+  100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+`
+
 // スタートボタン
 const StartButton = styled.a`
   display: block;
   position: relative;
   text-align: center;
-  width: 57.8vw;
+  width: 60vw;
   height: 3vh;
   margin: 0 auto;
   top: 55vh;
   background-color: transparent;
   text-decoration: none;
-  font-family: ${(props) => props.theme.fonts.subFont};
+  font-family: ${(props) => props.theme.fonts.fonts.sub};
   font-weight: 900;
   font-style: normal;
-  font-size: 1.5rem;
+  font-size: ${(props) => props.theme.fonts.sizes.body2};
   line-height: 29px;
   color: ${(props) => props.theme.colors.white};
   text-shadow: 3px 0 0 #2b2b2b, -3px 0 0 #2b2b2b, 0 -3px 0 #2b2b2b, 0 3px 0 #2b2b2b,
     3px 3px 0 #2b2b2b, -3px -3px 0 #2b2b2b, -3px 3px 0 #2b2b2b, 3px -3px 0 #2b2b2b;
 
   //アニメーション
-  animation: flash 2s linear infinite;
-  @keyframes flash {
-    0%,
-    100% {
-      opacity: 1;
-    }
-    50% {
-      opacity: 0;
-    }
-  }
+  animation: ${Flashing} 2s linear infinite;
 `
-
-// 一応残しておきました↓
-
-// const Parts: NextPage = () => {
-//   return (
-//     <Main>
-//       <h1>Button</h1>
-//       <div>
-//         <Button>Button</Button>
-//       </div>
-//       <div>
-//         <Button isActive>Button(Active)</Button>
-//       </div>
-//       <div>
-//         <Button isDisable>Button(Disabled)</Button>
-//       </div>
-//       <div>
-//         <Button isActive isDisable>
-//           Button(Active&Disabled)
-//         </Button>
-//       </div>
-//       <h1>PostCard</h1>
-//       <div>
-//         <PostCard
-//           postedDate={'2022年11月30日'}
-//           age={'20'}
-//           gender={'男'}
-//           content={'テキストが入ります'}
-//           isSelect={true}
-//           isFlamePost={false}
-//         ></PostCard>
-//       </div>
-//       <div>
-//         <PostCard
-//           postedDate={'2022年11月30日'}
-//           age={'20'}
-//           gender={'男'}
-//           content={'テキストが入ります'}
-//           isSelect={true}
-//           isFlamePost={true}
-//         ></PostCard>
-//       </div>
-//       <div>
-//         <PostCard
-//           postedDate={'2022年11月30日'}
-//           age={'20'}
-//           gender={'男'}
-//           content={'テキストが入ります'}
-//           isSelect={false}
-//           isFlamePost={false}
-//         ></PostCard>
-//       </div>
-//       <div>
-//         <PostCard
-//           postedDate={'2022年11月30日'}
-//           age={'20'}
-//           gender={'男'}
-//           content={'テキストが入ります'}
-//           isSelect={false}
-//           isFlamePost={true}
-//         ></PostCard>
-//       </div>
-//       <h1>DiamondShape</h1>
-//       <div style={{ display: 'flex' }}>
-//         <DiamondShape
-//           diagonal={200}
-//           diamondColor="green"
-//           borderColor="red"
-//           borderSize="5px"
-//           fontSize="20px"
-//         >
-//           aaaaa
-//         </DiamondShape>
-//         <DiamondShape
-//           diagonal={200}
-//           diamondColor="#D9D9D9"
-//           borderColor="#2B2B2B"
-//           borderSize="10px"
-//           fontSize="40px"
-//         >
-//           ?
-//         </DiamondShape>
-//       </div>
-//     </Main>
-//   )
-// }
