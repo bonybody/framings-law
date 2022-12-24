0e3ef33ae35655e698bf3f2dde8c3197acb3301e
kaho <57692753+tanakaho@users.noreply.github.com>
2022-12-24 23:40:05 +0900

    アイコンから?に変更

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
   return (
     <Container>
       <Anne />
-      <Logo src="/logo.svg" alt="logo" />
+      <Logo src="/question.svg" alt="logo" />
       <Title src="/titleLogo.svg" alt="title" />
       <StartButton>Tap to start game</StartButton>
     </Container>
@@ -26,8 +26,8 @@ const Container = styled.div`
 // ロゴ
 const Logo = styled.img`
   position: absolute;
-  width: 12.5vw;
-  height: 12.5vh;
+  width: 49.5px;
+  height: 49.5px;
   top: 6.5vh;
   right: 6.5vw;
 `
@@ -35,13 +35,13 @@ const Logo = styled.img`
 // アンネ
 const Anne = styled.div`
   position: absolute;
-  width: 100%;
-  height: 100%;
+  width: 108%;
+  height: 108%;
   /* aspect-ratio: 9/20; */
   /* top: 6.5vh; */
   background-image: url('/anne.svg');
-  background-position: top calc(10px - 0%) right 0%;
-  background-size: calc(1px + 110%) calc(1px + 120%);
+  background-position: top calc(10px + 52%) right 50%;
+  background-size: calc(1px + 105%) calc(1px + 110%);
   background-repeat: no-repeat;
 `
 
@@ -71,20 +71,15 @@ const StartButton = styled.a`
   display: block;
   position: relative;
   text-align: center;
-  width: 60vw;
-  height: 3vh;
-  margin: 0 auto;
   top: 55vh;
-  background-color: transparent;
-  text-decoration: none;
   font-family: ${(props) => props.theme.fonts.fonts.sub};
-  font-weight: 900;
-  font-style: normal;
+  font-weight: bold;
   font-size: ${(props) => props.theme.fonts.sizes.body2};
-  line-height: 29px;
   color: ${(props) => props.theme.colors.white};
-  text-shadow: 3px 0 0 #2b2b2b, -3px 0 0 #2b2b2b, 0 -3px 0 #2b2b2b, 0 3px 0 #2b2b2b,
-    3px 3px 0 #2b2b2b, -3px -3px 0 #2b2b2b, -3px 3px 0 #2b2b2b, 3px -3px 0 #2b2b2b;
+  text-shadow: 0 0 3px #2b2b2b, 0 0 3px #2b2b2b, 0 0 3px #2b2b2b, 0 0 3px #2b2b2b, 0 0 3px #2b2b2b,
+    0 0 3px #2b2b2b, 0 0 3px #2b2b2b, 0 0 3px #2b2b2b, 0 0 3px #2b2b2b, 0 0 3px #2b2b2b,
+    0 0 3px #2b2b2b, 0 0 3px #2b2b2b, 0 0 3px #2b2b2b, 0 0 3px #2b2b2b, 0 0 3px #2b2b2b,
+    0 0 3px #2b2b2b;
 
   //アニメーション
   animation: ${Flashing} 2s linear infinite;
