39f30bdbcee6169fdcc74fec007514676c15dc97
nishimura <11kakeru30@gmail.com>
2022-12-24 23:55:47 +0900

    backgroundの削除

--- frontend/src/components/Layout/MainLayout.tsx
+++ frontend/src/components/Layout/MainLayout.tsx
@@ -3,29 +3,10 @@ import styled from '@emotion/styled'
 import { ReactNode } from 'react'
 
 export type MainLayoutProps = {
-  isCharacterBackground?: boolean
   children: ReactNode
 }
 
-export const MainLayout = ({ isCharacterBackground = false, children }: MainLayoutProps) => {
-  if (isCharacterBackground) {
-    return (
-      <>
-        <Background>
-          <img src={'/background-chara.svg'} alt={''} />
-        </Background>
-
-        <UpperLeftTriangle>
-          <img src={'/rectangle-secondary.svg'} alt={''} />
-        </UpperLeftTriangle>
-        {children}
-        <BottomRightTriangle>
-          <img src={'/rectangle-primary.svg'} alt={''} />
-        </BottomRightTriangle>
-      </>
-    )
-  }
-
+export const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <>
       <UpperLeftTriangle>
@@ -40,27 +21,8 @@ export const MainLayout = ({ isCharacterBackground = false, children }: MainLayo
 }
 
 const triangleStyle = css`
-  width: calc(80px + 40vw);
+  width: calc(260px + 5vw);
   vertical-align: bottom;
-`
-
-const Background = styled.div`
-  position: fixed;
-  top: 20%;
-  left: 50%;
-  width: 100%;
-  height: 100vh;
-  z-index: -1000;
-  transform: translate(-50%, -50%);
-
-  /* background-image: url('/background-chara.svg');
-  background-size: cover;
-  background-position: center 40%;
-  background-repeat: no-repeat; */
-
-  img {
-    width: 100%;
-  }
 `
 
 const UpperLeftTriangle = styled.div`
@@ -70,8 +32,6 @@ const UpperLeftTriangle = styled.div`
   z-index: -999;
 
   img {
-    /* box-sizing: border-box; */
-    /* border-right: 10px solid #2b2b2b; */
     ${triangleStyle}
   }
 `
