67e012627238f1975c50448a1073c290a6c2d685
kaho <57692753+tanakaho@users.noreply.github.com>
2022-12-30 21:18:18 +0900

    変更取り込み

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,29 @@
+import aspida from '@aspida/axios'
+import Axios, { AxiosRequestHeaders } from 'axios'
+
+import { default as aspidaApi } from '@/api/$api'
+import { API_SERVER } from '@/config'
+
+const axiosInstance = Axios.create({
+  baseURL: API_SERVER
+})
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const message = error.response?.data?.message || error.message
+    console.error(message)
+    return Promise.reject(error)
+  }
+)
+
+export const api = (headers?: AxiosRequestHeaders) => {
+  return aspidaApi(
+    aspida(axiosInstance, {
+      headers: {
+        ...headers
+      }
+    })
+  )
+}
--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -3,6 +3,12 @@ import { css } from '@emotion/react'
 export const styles = css`
   html {
     font-family: 'Yusei Magic', sans-serif;
+    margin: 0;
+  }
+  ,
+  body {
+    margin: 0;
+    background: #d2104a;
   }
   body {
     margin: 0;
