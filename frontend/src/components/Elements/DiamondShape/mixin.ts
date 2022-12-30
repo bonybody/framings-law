5f6fbbfa779594bf989d82e2566ef06a06a57620
kaho <57692753+tanakaho@users.noreply.github.com>
2022-12-30 21:20:40 +0900

    変更取り込み

---
+++ frontend/src/components/Elements/DiamondShape/mixin.ts
@@ -0,0 +1,15 @@
+import { css } from '@emotion/react'
+
+export const mixinDiamondWrap = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const mixinDiamond = css`
+  transform: rotate(45deg);
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
