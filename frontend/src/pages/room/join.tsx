2d287a4ed758eb708d04890464cb01658cedb841
makotty <makotty.mako@gmail.com>
2023-01-04 18:57:05 +0900

    Create : Join Room Page

---
+++ frontend/src/pages/room/join.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+import { JoinRoom } from '@/module/room/components/StartRoom'
+
+const JoinRoomPage: NextPage = () => {
+  return <JoinRoom />
+}
+
+export default JoinRoomPage
