e3e6062710f55be30e17555e34558613073adc0d
D_nishimura <11kakeru30@gmail.com>
2023-01-05 20:13:06 +0900

    update: apiを動作させられるように更新

--- frontend/src/module/game/api/getGameJoinUser.tsx
+++ frontend/src/module/game/api/getGameJoinUser.tsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import { useQuery } from 'react-query'
 
 import { ApiHookState } from '@/@types'
-import { ApiClientOptions } from '@/lib/api'
+import { GamePlayer } from '@/api/@types'
+import { apiClient, ApiClientOptions } from '@/lib/api'
 
-// import { apiClient, ApiClientOptions } from '@/lib/api'
 import { GameId } from '../types'
 
 type GetGameJoinUserOptions = Pick<ApiClientOptions, 'idToken'> & {
@@ -12,92 +12,81 @@ type GetGameJoinUserOptions = Pick<ApiClientOptions, 'idToken'> & {
 }
 
 export const getGameJoinUser = async ({ idToken, gameId }: GetGameJoinUserOptions) => {
-  // const res = await apiClient({ idToken }).games._gameId(gameId).players.$get()
+  const { players } = await apiClient({ idToken })
+    .games._gameId_string(gameId as string)
+    .players.$get()
   console.log(idToken)
-  const res = {
-    players: [
-      {
-        id: '1',
-        userId: '1',
-        gameId: gameId,
-        isFramer: true,
-        character: {
-          id: 'id',
-          displayName: 'anne',
-          imageUrl: 'anne'
-        }
-      },
-      {
-        id: '2',
-        userId: '2',
-        gameId: gameId,
-        isFramer: true,
-        character: {
-          id: 'id',
-          displayName: 'anne',
-          imageUrl: 'anne'
-        }
-      },
-      {
-        id: '3',
-        userId: '3',
-        gameId: gameId,
-        isFramer: true,
-        character: {
-          id: 'id',
-          displayName: 'anne',
-          imageUrl: 'anne'
-        }
-      },
-      {
-        id: '4',
-        userId: '4',
-        gameId: gameId,
-        isFramer: true,
-        character: {
-          id: 'id',
-          displayName: 'anne',
-          imageUrl: 'anne'
-        }
-      },
-      {
-        id: '5',
-        userId: '5',
-        gameId: gameId,
-        isFramer: true,
-        character: {
-          id: 'id',
-          displayName: 'anne',
-          imageUrl: 'anne'
-        }
-      }
-    ]
-  }
+  // const res = {
+  //   players: [
+  //     {
+  //       id: '1',
+  //       userId: '1',
+  //       gameId: gameId,
+  //       isFramer: true,
+  //       character: {
+  //         id: 'id',
+  //         displayName: 'anne',
+  //         imageUrl: 'anne'
+  //       }
+  //     },
+  //     {
+  //       id: '2',
+  //       userId: '2',
+  //       gameId: gameId,
+  //       isFramer: true,
+  //       character: {
+  //         id: 'id',
+  //         displayName: 'anne',
+  //         imageUrl: 'anne'
+  //       }
+  //     },
+  //     {
+  //       id: '3',
+  //       userId: '3',
+  //       gameId: gameId,
+  //       isFramer: true,
+  //       character: {
+  //         id: 'id',
+  //         displayName: 'anne',
+  //         imageUrl: 'anne'
+  //       }
+  //     },
+  //     {
+  //       id: '4',
+  //       userId: '4',
+  //       gameId: gameId,
+  //       isFramer: true,
+  //       character: {
+  //         id: 'id',
+  //         displayName: 'anne',
+  //         imageUrl: 'anne'
+  //       }
+  //     },
+  //     {
+  //       id: '5',
+  //       userId: '5',
+  //       gameId: gameId,
+  //       isFramer: true,
+  //       character: {
+  //         id: 'id',
+  //         displayName: 'anne',
+  //         imageUrl: 'anne'
+  //       }
+  //     }
+  //   ]
+  // }
 
-  return await Promise.resolve(res)
-  // return res
+  // return await Promise.resolve(res)
+  return players
 }
 
 type UseGameJoinUserState = ApiHookState & {
-  userList:
-    | undefined
-    | {
-        players: {
-          id: string
-          userId: string
-          gameId: string | number
-          isFramer: boolean
-          character: {
-            id: string
-            displayName: string
-            imageUrl: string
-          }
-        }[]
-      }
+  userList: GamePlayer[]
 }
+
 export const useGameJoinUser = (options: GetGameJoinUserOptions) => {
   const [state, setState] = useState<UseGameJoinUserState>({
-    userList: undefined,
+    userList: [],
     isLoading: true
   })
 
